6f04000c202e66a63e276ef393453052784cd4b3
John Michael Goco <jmgoco416@gmail.com>
2025-02-25 14:32:23 +0800

    Added Login Page

--- app/src/main/java/com/moneymate/MainActivity.java
+++ app/src/main/java/com/moneymate/MainActivity.java
@@ -1,6 +1,8 @@
 package com.moneymate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
 
 //  Variables
     Animation logoAnim;
@@ -31,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
 
 //      Apply the animation to the ImageView
         logo.setAnimation(logoAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
 
 //      Handle window insets to adjust padding for system bars (status bar, navigation bar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
