37822a265f03b308e253565d04903548f75ac2eb
John Michael Goco <jmgoco416@gmail.com>
2025-02-25 15:52:04 +0800

    Created SignUp Layout

--- app/src/main/java/com/moneymate/Login.java
+++ app/src/main/java/com/moneymate/Login.java
@@ -1,6 +1,9 @@
 package com.moneymate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,24 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        // Handle system insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Find the "Sign Up" button
+        Button signUpButton = findViewById(R.id.signUpBtn);
+
+        // Set click listener to go to SignUp page
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
         });
     }
 }
