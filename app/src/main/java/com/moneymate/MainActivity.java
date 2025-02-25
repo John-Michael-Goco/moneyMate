21986f8b2651a365f764232f56657e3660c71166
John Michael Goco <jmgoco416@gmail.com>
2025-02-25 13:41:46 +0800

    Added Splash Screen and Animation

--- app/src/main/java/com/moneymate/MainActivity.java
+++ app/src/main/java/com/moneymate/MainActivity.java
@@ -1,6 +1,9 @@
 package com.moneymate;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+//  Variables
+    Animation logoAnim;
+    ImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+//      Load animation from XML
+        logoAnim = AnimationUtils.loadAnimation(this, R.anim.logo_anim);
+
+//      Find the ImageView in the layout
+        logo = findViewById(R.id.logo);
+
+//      Apply the animation to the ImageView
+        logo.setAnimation(logoAnim);
+
+//      Handle window insets to adjust padding for system bars (status bar, navigation bar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
